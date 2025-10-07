2440a8ffff94de88ad72e5ea2e2e6f943aba5bb4
Caleb Muthama <calebnzioka2027@gmail.com>
2025-10-07 10:08:09 +0300

    Google Dependency Issues

--- backend/chats/views.py
+++ backend/chats/views.py
@@ -1,6 +1,6 @@
 
-from google import genai
-from google.genai import types
+import google.generativeai as genai
+from google.generativeai import types
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
