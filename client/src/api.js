5818d7bd53e8f4a9e4bd9b77b81e55516315b523
Caleb Muthama <calebnzioka2027@gmail.com>
2025-10-10 12:40:44 +0300

    deployment issues

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_BASE = "https://ai-assistant-d27n.onrender.com/api/";
-const API_BASE = "http://127.0.0.1:8000/api/";
+const API_BASE = "https://ai-assistant-d27n.onrender.com/api/";
+// const API_BASE = "http://127.0.0.1:8000/api/";
 
 
 const api = axios.create({
